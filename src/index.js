3446611a65150b6f7f17fbe9082271c78dab6241
doocubii <doocubii@gmail.com>
2022-12-16 22:23:00 +0900

    customdict 추가

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@
 module.exports.BaikalLanguageServiceClient = require("./BaikalLanguageServiceClient.js");
 module.exports.CustomDictionaryServiceClient = require("./CustomDictionaryServiceClient.js")
 module.exports.Tagger = require("./Tagger.js")
+module.exports.CustomDict = require("./CustomDict.js")
