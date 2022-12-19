2e870bbe60e6c20139c6825bc97f8d89c0a87e20
doocubii <doocubii@gmail.com>
2022-12-20 01:41:25 +0900

    불필요한 내용 삭제.

--- src/ClientBase.js
+++ src/ClientBase.js
@@ -36,20 +36,7 @@ class ClientBase {
         
         this.remote = remote;
 
-        // let packageDefinition = protoLoader.loadSync(
-        //     protofile,
-        //     {
-        //         keepCase: true,
-        //         longs: String,
-        //         enums: String,
-        //         defaults: true,
-        //         oneofs: true,
-        //         includeDirs: conf.protos.includeDirs
-        //       });
-
-
         this.proto = ClientBase.getProto(protofile) ;
-        // grpc.loadPackageDefinition(packageDefinition).bareun;
 
         this.client = new this.proto[service](
             this.remote,
@@ -58,38 +45,7 @@ class ClientBase {
     }
 
 
-/*
-    error(callback) {
-        this.error_callback = callback;
-        return this;
-    }
-    done(callback) {
-        this.done_callback = callback;
-        return this;
-    }
 
-    proc_response(err, res) {
-        let _err = this.error_callback;
-        this.error_callback = null;
-        let _done = this.done_callback;
-        this.done_callback = null;
-
-        if( err ) {
-            if(!_err) {
-                console.log("ERROR : error_callback is null.")
-                return ;
-            }
-            _err(err);
-        } 
-        else {
-            if(!_done) {
-                console.log("ERROR : done_callback is null.")
-                return ;
-            }
-            _done(res);
-        }
-    }
-    */
 
 }
 
